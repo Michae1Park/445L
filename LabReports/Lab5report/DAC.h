8ec5ce8e92e765a94d953700e33234203da155a7
Michael Park <michael.j.park@utexas.edu>
2016-02-25 03:33:56 -0600

    prelab5 SW

---
+++ LabReports/Lab5report/DAC.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+// **************DAC_Out*********************
+// DAC output
+// Input: 4 bits to output to DAC
+void DAC_Out(uint16_t output);
+
+// **************DAC_Init*********************
+// Initialize DAC
+void DAC_Init(uint16_t data);
---
+++ LabReports/Lab5report/Switch.h
@@ -0,0 +1,5 @@
+//------------Switch_Init------------
+// Initialize GPIO Port C bit 4-7 for input
+// Input: none
+// Output: none
+void Switch_Init(void);
